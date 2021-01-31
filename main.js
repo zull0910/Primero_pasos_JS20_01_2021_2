62af304f967d34deeaec51496cceab19f1b78ac7
Sergio David Medina Mateus <smedina09101124@gmail.com>
2021-01-30 19:23:26 -0500

    Tipos de Datos 2

---
+++ main.js
@@ -0,0 +1,41 @@
+// Tipos de Datos 
+
+// string o cadena
+
+"Sergio David"
+"Medina Mateus"
+
+
+// number numero
+
+1000
+2.5
+-25
+
+
+// boolean afirmaciones
+false
+true
+
+// array listas
+['Sergio', 'David', 'Medina']
+[1, 2, 3, 4]
+[true, false, true, false]
+
+
+// object objeto
+{
+    nombre: "Sergio",
+    apellido: "Medina",
+    edad: 33,
+    profesor: false
+
+}
+{
+    nombre: "Andres",
+    apellido: "Castro",
+    edad: 20,
+    profesor: null
+
+}
+
